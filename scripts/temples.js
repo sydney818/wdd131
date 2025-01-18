dff0d0b9f7898953c6db2531b8931d596227a624
sydney818 <sydney818@gmail.com>
2025-01-17 22:37:35 -0700

    Week03 initial commit

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+document.querySelector("#currentyear").textContent = today.getFullYear();
+
+
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+
