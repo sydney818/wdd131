341133cf688968c50f2dc18bdda3a0f4524c9cce
sydney818 <sydney818@gmail.com>
2025-01-28 23:44:47 -0700

    Week04 first commit

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,14 @@
+const today = new Date();
+
+document.querySelector("#currentyear").textContent = today.getFullYear();
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+
+const mainnav = document.querySelector('nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
