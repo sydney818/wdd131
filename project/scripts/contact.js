c106cf137aee7d5344317c37b6e93f78f5b458ae
sydney818 <sydney818@gmail.com>
2025-02-14 20:34:27 -0700

    test

--- project/scripts/contact.js
+++
@@ -1,5 +0,0 @@
-// Hamburger menu
-document.getElementById("hamburger-btn").addEventListener("click", function() {
-    const nav = document.querySelector("nav");
-    nav.classList.toggle("active");
-});
